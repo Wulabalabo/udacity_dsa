958e3dac593c3a7a68511dad00ae82c4c7efaefe
JasonChen <138670424@qq.com>
2022-11-08 10:26:39 +0800

    feat: task1 completed

--- Unscramble_computer_sience_problems/Task1.py
+++ Unscramble_computer_sience_problems/Task1.py
@@ -10,11 +10,23 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
-
 """
 TASK 1:
 How many different telephone numbers are there in the records? 
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+def getDifferentNumbers()->set:
+    diff = set()
+
+    for eachNumbers in [0,1]:
+        diff_texts = [data[eachNumbers] for data in texts]
+        diff.update(diff_texts)
+        diff_calls = [data[eachNumbers] for data in calls]
+        diff.update(diff_calls)
+
+    return diff
+
+if __name__ == '__main__':
+    differentNumbers = getDifferentNumbers()
+    print("There are {0} different telephone numbers in the records.".format(len(differentNumbers)))
