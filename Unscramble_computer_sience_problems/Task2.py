633e10beada4dab61e06db82bc74f42a49955ba0
JasonChen <138670424@qq.com>
2022-11-11 10:33:35 +0800

    feat: completed task2

--- Unscramble_computer_sience_problems/Task2.py
+++ Unscramble_computer_sience_problems/Task2.py
@@ -2,7 +2,9 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
+
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -11,6 +13,22 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+
+from collections import defaultdict
+from datetime import datetime
+
+if __name__ == '__main__':
+    calls_dictionary = defaultdict(int)
+
+
+for caller, reciever, timestamp, duration in calls:
+    date = datetime.strptime(timestamp, "%d-%m-%Y %H:%M:%S")
+    if date.year == 2016 and date.month == 9:
+        calls_dictionary[caller] += int(duration)
+        calls_dictionary[reciever] += int(duration)
+
+highest_duration = max(calls_dictionary.items(), key=lambda x: x[1])
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(*highest_duration))
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
